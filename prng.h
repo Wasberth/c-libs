8f16ea302934573d67e78acc001166b2a2c9afa2
Wasberth <wdavidgp@hotmail.com>
2022-03-21 14:41:36 -0600

    randomLib

---
+++ prng.h
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <limits.h>
+#include <math.h>
+
+#define a 1664525UL
+#define c 1013904223UL
+#define m INT_MAX
+
+unsigned int seed = 0;
+
+void changeSeed(unsigned int newSeed) {
+    seed = newSeed;
+}
+
+unsigned int nextRand() {
+    unsigned long s = ((a * (unsigned long) seed + c)) % m;
+    seed = (unsigned int) s;
+    return seed;
+}
+
+unsigned int nextRandLim(int min, int max) {
+    return nextRand() % (max - min + 1) + min;
+}
+
+double nextDouble() {
+    return ((double) nextRand()) / m;
+}
+
+unsigned int nextBool() {
+    return nextDouble() < 0.5;
+}
+
+unsigned char nextChar() {
+    return nextRand() % UCHAR_MAX;
+}
